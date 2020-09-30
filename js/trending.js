a144f2d430c5c295f3b43809973acc5e0b1df2c9
AgostinaArevaloJaube <agosarevalo@gmail.com>
2020-09-30 20:23:55 -0300

    Ordena archivos

--- js/trending.js
+++ js/trending.js
@@ -1,3 +1,4 @@
+// ----------------------  Trending Styles ----------------------  \\
 // Setea los botones previo y next, de acuerdo al estado y al theme elegido
 const setTrendingBtn = () => {
 	if (localStorage.getItem('dark-mode') === 'true') {
@@ -19,3 +20,43 @@ $nextBtn.addEventListener('mouseover', () => {
 
 $previousBtn.addEventListener('mouseout', setTrendingBtn);
 $nextBtn.addEventListener('mouseout', setTrendingBtn);
+
+
+// ----------------------  Trending API ----------------------  \\
+
+const getTrendingGif = async () => {
+	await fetch(`${trendingEndpoint}?api_key=${apiKey}&limit=12&rating=g`)
+		.then((response) => response.json())
+		.then((trendings) => {
+			console.log(trendings);
+			displayTrendingGifs(trendings);
+		})
+		.catch((err) => console.error(err));
+};
+
+getTrendingGif();
+
+
+const displayTrendingGifs = (trendings) => {
+	for (let i = 0; i < trendings.data.length; i++) {
+		const gifContainer = document.createElement('div');
+		gifContainer.classList.add('gif__container');
+		gifContainer.innerHTML = ` 
+		<img class="gif" src="${trendings.data[i].images.original.url}" alt="${trendings.data[i].title}">
+	
+		<div class="gifActions">
+			<div class="gifActions__btn">
+				<img src="assets/icon-fav.svg" class="favorite" alt="Botón para agregar a mis favoritos">
+				<img src="assets/icon-download.svg" class="download" alt="Botón para descargar">
+				<img src="assets/icon-max-normal.svg" class="maximize" alt="Botón para maximizar">
+			</div>
+			<div class="gif__info">
+				<p class="gif_user">${trendings.data[i].username}</p>
+				<p class="gif_title">${trendings.data[i].title}</p>
+			</div>
+		</div>
+		`;
+		$trendingSlider.appendChild(gifContainer);
+	}
+}
+
